93373b2dc0692065dd985828103b488041349997
AntonioLaurance <74690107+AntonioLaurance@users.noreply.github.com>
2022-08-16 22:01:39 -0500

    Se agregó una nueva forma de ingresar n

--- Summatory/main.cpp
+++ Summatory/main.cpp
@@ -8,18 +8,28 @@
 #include <iostream>
 #include "functions.h"
 
+using namespace std;
+
 int main(int argc, char *argv[]){
 	int n;
 
-	cout << "Sumatoria de 1 hasta n" << endl;
-	cout << "n(número entero): ";
-	cin >> n;
-
+	if(argc < 2 ){
+		// n ingresado manualmente 
+		cout << "Sumatoria de 1 hasta n" << endl;
+		cout << "n(número entero): ";
+		cin >> n;
+	} else{
+		// n ingresado como parámetro de programa
+		n = atoi(argv[1]); 
+	}
+	
+	cout << endl;
 	cout << "Sumatoria de 1 hasta " << n << endl;
+	cout << endl;
 
 	cout << "Suma iterativa: " << sumaIterativa(n) << endl;
 	cout << "Suma recursiva: " << sumaRecursiva(n) << endl;
-	cout << "Suma directa: " << sumaDirecta(n) << endl;
+	cout << "Suma directa: " << sumaDirecta(n) << endl << endl;
  
 	return 0;
 }
