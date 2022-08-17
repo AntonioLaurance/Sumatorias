43753e14bf7c2f330804108963769b7905e5539d
AntonioLaurance <74690107+AntonioLaurance@users.noreply.github.com>
2022-08-17 18:59:54 -0500

    Ampliación de la documentación

--- Summatory/functions.h
+++ Summatory/functions.h
@@ -1,11 +1,19 @@
 /* Funciones de Suma de números enteros de 1 hasta n
- * En este módulo ...
+ * En este módulo se incluyen tres funciones que hacen
+ * la sumatoria a través de diferentes métodos
 */
 
 /*
- * @params:
+ * @params: 
+ * 		n: Es un número entero positivo
+ * 
+ * @return:
+ * 		sum: Número entero positivo obtenido a través de la suma
+ * 		total de números consecutivos comenzando en 1 hasta llegar a n
+ * 		
  *
  * Complejidad algoritmica temporal única: T(n) = 3n + 3
+ * Complejidad algoritmica espacial: S(n) = 
 */
 unsigned long int sumaIterativa(unsigned int n){
 	unsigned long int sum = 0;	// Acumulador 
@@ -18,9 +26,16 @@ unsigned long int sumaIterativa(unsigned int n){
 
 /*
  * @params:
- *
- * Complejidad algoritmica temporal para peor de los casos: 	T(n) = n
+ * 		n: Es un número entero positivo
+ * 
+ * @return:
+ * 		Número entero positivo obtenido a través de la suma total de números
+ * 		consecutivos comenzando en 1 hasta llegar a n
+ * 
+ * Complejidad algoritmica temporal para el peor de los casos: 	T(n) = n
  * Complejidad algoritmica temporal para el mejor de los casos: T(n) = 0
+ * 
+ * Complejidad algoritmica espacial: S(n) = 
 */
 unsigned long int sumaRecursiva(unsigned int n){
 	// Caso base 
@@ -32,11 +47,19 @@ unsigned long int sumaRecursiva(unsigned int n){
 }
 
 /*
- * @params: 
+ * Se utilizo la fórmula de Gauss para calcular esta sumatoria
  *
+ * @params: 
+ * 		n: Es un número entero positivo
+ *
+ * @return:
+ * 		Número entero positivo obtenido a través de la suma total de números
+ * 		consecutivos comenzando en 1 hasta llegar a n
+ * 		
  * Complejidad algoritmica temporal única: T(n) = 1
+ * Complejidad algoritmica espacial: S(n) = 
 */
-/* Formula de Gauss para las sumatorias */
+
 unsigned long int sumaDirecta(unsigned int n){
 	return static_cast<unsigned long int>((n * (n + 1))/2);
 }
