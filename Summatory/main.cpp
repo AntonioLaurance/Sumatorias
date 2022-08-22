1afab4935ed475bedfce817c58ae56c88d278583
AntonioLaurance <74690107+AntonioLaurance@users.noreply.github.com>
2022-08-21 21:52:16 -0500

    Adición de comentarios y uso de una función de time.h para obtener los tiempos en los que se tarda en realizar cada sumatoria

--- Summatory/main.cpp
+++ Summatory/main.cpp
@@ -2,6 +2,7 @@
  * Actividad 1.1: Funciones Iterativas, Recursivas y su análisis de Complejidad
  * Victoria Rodríguez de León			A01656328
  * Ricardo Campos Luna					A01656898
+ * Raúl Armando Vélez Robles			A01782488
  * Martes 16 de agosto del 2022
 */
 
@@ -13,6 +14,9 @@ using namespace std;
 
 int main(int argc, char *argv[]){
 	unsigned int n;
+	unsigned long int sum1, sum2, sum3;
+	clock_t begin, diff, end; 
+	
 
 	if(argc < 2 ){
 		// n ingresado manualmente 
@@ -29,9 +33,29 @@ int main(int argc, char *argv[]){
 	cout << "Sumatoria de 1 hasta " << n << endl;
 	cout << endl;
 
-	cout << "Suma iterativa: " << sumaIterativa(n) << endl;
-	cout << "Suma recursiva: " << sumaRecursiva(n) << endl;
-	cout << "Suma directa: " << sumaDirecta(n) << endl << endl;
+	// Caso Iterativo
+	begin = clock();
+	sum1 = sumaIterativa(n);
+	end = clock();
+	diff = end - begin;
+	
+	cout << "Suma iterativa: " << sumaIterativa(n) << "\t" << "Tiempo: " << diff << endl;
+
+	// Caso Recursivo
+	begin = clock();
+	sum2 = sumaRecursiva(n);
+	end = clock();
+	diff = end - begin;
+
+	cout << "Suma recursiva: " << sumaRecursiva(n) << "\t" << "Tiempo: " << diff << endl;
+
+	// Caso Directo
+	begin = clock();
+	sum3 = sumaDirecta(n);
+	end = clock();
+	diff = end - begin;
+
+	cout << "Suma directa: " << sumaDirecta(n) << "\t" << "Tiempo: " << diff << endl << endl;	
  
 	return 0;
 }
