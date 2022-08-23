679aba97f1a9b2b948d17e04ac3fc91e1b44ca91
AntonioLaurance <74690107+AntonioLaurance@users.noreply.github.com>
2022-08-22 20:38:56 -0500

    Adición de test cases

--- Summatory/main.cpp
+++ Summatory/main.cpp
@@ -1,20 +1,38 @@
 /* Programación de Estructuras de datos y Algoritmos Fundamentales
  * Actividad 1.1: Funciones Iterativas, Recursivas y su análisis de Complejidad
  * Victoria Rodríguez de León			A01656328
- * Ricardo Campos Luna				A01656898
+ * Ricardo Campos Luna				    A01656898
  * Raúl Armando Vélez Robles			A01782488
- * Martes 16 de agosto del 2022
+ * Lunes 22 de agosto del 2022
 */
 
 // Instituto Tecnológico y de Estudios Superiores de Monterrey (ITESM)
+// Profesor: Francisco Javier Vázquez Gómez
 
 /* ------------------------------------------------------------------------------
  * 								    TEST CASES
  * ------------------------------------------------------------------------------
- *  1) En el método directo cuando la cantidad ingresada es muy grande 
- *  2) Cuando la cantidad ingresada es negativa 
- *  3) Cuando el usuario ingresa otra cosa que no son valores numéricos 
+ *  1) Cuando la cantidad ingresada es muy grande, se provoca
+ * 	desbordamiento de memoria (Stack Overflow) en el caso recursivo, los otros casos 
+ *  funcionan bien mientras el resultado de la operación sea menor que lo que puede 
+ * 	almacenar el tipo de dato long unsigned int que es 2^64 - 1 (generalmente). 
  * 
+ *  2) Cuando la cantidad ingresada es negativa, nuestro programa hace una reinterpretación
+ *  binaria para convertirlo en un tipo unsigned int, sin embargo esta conversión es 
+ * 	2^32 - [El valor absoluto del número insertado] y cuando el número negativo está más 
+ *  cercano al 0, más cercano estará su conversión al número 2^32
+ * 
+ *  3) Cuando el usuario ingresa otra cosa que no son valores numéricos, el caso iterativo y
+ *  matemático dan 0 y el recursivo provoca un desbordamiendo de memoria (Stack Overflow).
+ * 
+ *  4) Cuando el tiempo en ejecutar un algoritmo es muy pequeño el sistema redondea el
+ *  tiempo a 0 y por eso para cantidades muy chicas en el método matemático se puede ver
+ *  que el programa dice que el tiempo tardado es 0
+ * 
+ *	El usuario debe ingresar para n una cantidad menor que: 4,294,967,295 = 2^32 - 1
+ * 
+ *  Nota: El desbordamiento de memoria generalmente provoca que el sistema operativo aborte 
+ * 	este programa. 
 */
 
 // ¿Ocupamos manejo de excepciones?
